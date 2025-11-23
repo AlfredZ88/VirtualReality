dae4b4b6d04e1a1b50edb331fc402fe491733283
AlfredZ88 <Alfredz8@nycstudents.net>
2025-11-23 21:00:25 +0000

    UPD 2 PRAC 3_2

---
+++ VRPractical3_2/UFO.js
@@ -0,0 +1,19 @@
+class UFO{
+  constructor(x,y,z){
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.dz = .2;
+    this.fall = false;
+
+    this.obj = ufo.cloneNode(true);
+    this.obj = document.
+    this.obj.setAttribute("position",{x:x,y:y,z:z});
+    scene.append(this.obj);
+
+
+    invade()
+
+}
+}
+    
--- VRPractical3_2/script.js
+++ VRPractical3_2/script.js
@@ -1,16 +1,38 @@
+let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
 let scene;
-
+let car, UFO = [];
 
 window.addEventListener("DOMContentLoaded",function() {
-  scene = document.querySelector("a-scene")
+  scene = document.querySelector("a-scene");
+  car = document.getElementById("car");
+  UFO = document.getElementById("UFO");
+  car.g = -15;
+  car.gr = .01;
+  UFO.y = 10;
+  UFO.dy= -.1;
+  UFO.fall = false;
+  
+  car.addEventListener("click", function(){
+    car.drive = true;
+  })
 
+  UFO.addEventListener("mouseenter", function(){
+    UFO.fall = true;
+  })
 
-      
   loop();
 })
 
 function loop(){
-
-    
+  // click the body of the car to make it drive
+  if(car.drive){
+    car.g += car.gr;
+    car.setAttribute("position", {x:car.g, y:1, z:0});
+  }
+  
+  if(UFO.fall){
+    UFO.y += UFO.dy
+    UFO.setAttribute("position",{x:0, y:UFO.y, z:0})
+  }
   window.requestAnimationFrame(loop);
 }
